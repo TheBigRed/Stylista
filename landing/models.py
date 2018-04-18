e717c86494a1fab0b64571b7fb0e4db01aff0493
Unknown <ramraja.vii@gmail.com>
2018-04-18 01:14:50 -0400

    Create stylist model class
    *change timezone
    *Migrate django apps & databases

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Stylist(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    joined_date = models.DateTimeField('date joined')
+    email = models.EmailField(max_length=50)
