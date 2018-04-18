c3eafe1684f02b7060abfa562b1c9d0d7b31868a
Unknown <ramraja.vii@gmail.com>
2018-04-18 03:02:39 -0400

    Create detail view
    *View to show stylist id
    *Add url to landing/urls.py

--- landing/views.py
+++ landing/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, stylist_id):
+    return HttpResponse("You're looking at question %s." % stylist_id)
