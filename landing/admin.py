75ffe5b15dd2b9be530a9cb20328fe8b7f2698cc
Unknown <ramraja.vii@gmail.com>
2018-04-18 02:19:52 -0400

    Add Stylist (database) to admin page

--- landing/admin.py
+++ landing/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Stylist
+
+admin.site.register(Stylist)
--- landing/models.py
+++ landing/models.py
@@ -6,3 +6,6 @@ class Stylist(models.Model):
     last_name = models.CharField(max_length=25)
     joined_date = models.DateTimeField('date joined')
     email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return self.first_name
