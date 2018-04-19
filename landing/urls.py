c94c44ef867f8faf324386621dcb9819316bcefe
Unknown <ramraja.vii@gmail.com>
2018-04-19 00:56:29 -0400

    Add name view
    *Get names of stylists and display in view
    *Add name view to url path

--- landing/urls.py
+++ landing/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # /landing/1
     path('<int:stylist_id>/', views.detail, name='detail'),
+    # /landing/name
+    path('name/', views.name, name='name'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from .models import Stylist
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
 def detail(request, stylist_id):
     return HttpResponse("You're looking at question %s." % stylist_id)
+
+
+def name(request):
+    name_styl = Stylist.objects.order_by('-joined_date')
+    return HttpResponse(name_styl)
