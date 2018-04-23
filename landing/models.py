3c2e0b023ec6c26540e2dc48244cd8b57eabc259
Unknown <ramraja.vii@gmail.com>
2018-04-23 03:27:20 -0400

    Create password field in model & migrate database changes

--- landing/models.py
+++ landing/models.py
@@ -6,6 +6,7 @@ class Stylist(models.Model):
     last_name = models.CharField(max_length=25)
     joined_date = models.DateTimeField('date joined')
     email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=20, default='')
 
     def __str__(self):
         return self.first_name
