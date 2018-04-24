c433b467f7a2f346d4697f6dd4efb52aa8e3046c
Unknown <ramraja.vii@gmail.com>
2018-04-24 19:15:14 -0400

    Admin display fields
    *Display more fields from the stylista object

--- landing/admin.py
+++ landing/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-
 from .models import Stylist
 
-admin.site.register(Stylist)
+
+class StylistaAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'joined_date')
+
+
+admin.site.register(Stylist, StylistaAdmin)
