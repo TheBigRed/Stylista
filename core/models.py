af5008c7187a11486652e4c7ec56bf5f64002e8a
Unknown <ramraja.vii@gmail.com>
2018-05-17 06:28:28 -0400

    Add usersetting model and modify Account model
    *Add more fields to Account model
    *Add UserSetting model

--- core/models.py
+++ core/models.py
@@ -4,9 +4,28 @@ from core.forms import UploadFileForm
 
 
 class Account(models.Model):
+
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    STYLISTA_TYPES = (
+        ('BARBER', 'Barber'),
+        ('HAIR_STYLIST', 'Hair Stylist'),
+        ('MAKEUP', 'Makeup Artist'),
+        ('MUAH', 'Make Up And Hair'),
+        ('NAILS', 'Nail Technician'),
+    )
+
     account_holder = models.ForeignKey(Stylist, models.CASCADE, related_name='account')
-    stylist_type = models.CharField(max_length=25, blank=True)
+    gender = models.CharField(max_length=25, choices=GENDER, blank=True)
+    stylist_type = models.CharField(max_length=25, choices=STYLISTA_TYPES, blank=True)
+    address = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=25, blank=True)
     location = models.CharField(max_length=25, blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    about_me = models.TextField(blank=True)
 
     def user_directory_path(self, store_front):
         # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
--- usersettings/models.py
+++ usersettings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from landing.models import Stylist
 
-# Create your models here.
+
+class UserSetting(models.Model):
+    account_holder = models.ForeignKey(Stylist, models.CASCADE, related_name='settings_account')
+    notifications = models.BooleanField()
+    push_notifications = models.BooleanField()
+    text_notifications = models.BooleanField()
+    promotions = models.BooleanField()
+    search_engine = models.BooleanField()
+    deactivate = models.BooleanField()
+    time_zone = models.CharField(max_length=15)
+    languages = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self
