5d4acc00d178abdf9659dababa4fd7d9150e95b7
Unknown <ramraja.vii@gmail.com>
2018-05-22 04:31:33 -0400

    Get django variables from settings form placeholders
    *Transfer placeholder variables and put them into input fields
    *Create input iterator
    *Add class d-none when new settings tab is clicked

--- usersettings/static/usersettings/js/main.js
+++ usersettings/static/usersettings/js/main.js
@@ -3,10 +3,24 @@ $(document).ready(function() {
     $('.list-group li').click(function(e) {
         e.preventDefault()
 
-        $that = $(this);
+        //$that = $(this);
 
         $('.list-group').find('li').removeClass('active');
-        $that.addClass('active');
+        $(this).addClass('active');
+        $('#setting-form').addClass('d-none')
     });
 
+    loadInputValues();
+
+
 });
+
+
+//Iterate over all input fields in form and put placeholder value as input value
+function loadInputValues() {
+
+    $('#setting-form *').filter(':input').each(function(){
+        var ph = $(this).attr('placeholder');
+        $(this).val(ph);
+    });
+}
