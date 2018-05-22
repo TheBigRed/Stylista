1236383447d13dd429082c745ce5e93ea0a99764
Unknown <ramraja.vii@gmail.com>
2018-05-22 02:00:47 -0400

    Create update account view for Settings app
    *Add form call to updateaccount view
    *Add updateaccount url
    *Create updateaccount view
    *Get usersession and update the account associated with the user_pk

--- usersettings/urls.py
+++ usersettings/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'usersettings'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('updateaccount', views.updateaccount, name='updateaccount'),
 ]
--- usersettings/views.py
+++ usersettings/views.py
@@ -1,6 +1,30 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render
+from landing.utils import get_dbsession
+from django.urls import reverse
+from landing.models import Stylist
+from core.models import Account
 
 
 def index(request):
     return render(request, 'usersettings/settings.html')
+
+
+def updateaccount(request):
+    '''
+    :param request: get request which has user on db session stored in session cookie
+    :return: main page with correct user after authenticate session
+    '''
+    if request.session.keys():
+        s = get_dbsession(request.session['session_login'])
+        stylist = Stylist.objects.get(pk=s['user_pk'])
+        account = Account.objects.get(account_holder_id=s['user_pk'])
+        Account.objects.filter(pk=account.pk).update(address='updated')
+        print("Received cookie session: {}".format(request.session.session_key))
+        print("Update session for : {}".format(stylist.first_name))
+        print("Account session for: {}".format(account.account_holder))
+        request.session.cycle_key()
+
+        return HttpResponse("Updated account")
+    else:
+        return HttpResponseRedirect(reverse('landing:login'))
