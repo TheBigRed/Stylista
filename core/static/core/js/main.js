cb844db5b9b5d2fc33e6baf997dc22d1a0e0918e
Unknown <ramraja.vii@gmail.com>
2018-05-30 17:59:11 -0400

    Add time picker
    *Add clock icon
    *Add time picker to availability box
    *Create js function for timepicker

--- core/static/core/js/main.js
+++ core/static/core/js/main.js
@@ -47,4 +47,44 @@ $(document).ready(function() {
 
     });
 
+    $("#timepicker").flatpickr({
+
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H:i",
+        time_24hr: true/*
+        onChange: function(selectedDates, dateStr, instance) {
+        //...
+        },
+        onOpen: [
+            function(selectedDates, dateStr, instance){
+
+
+
+        },
+            function(selectedDates, dateStr, instance){
+
+        }
+        ],
+        onClose: function(selectedDates, dateStr, instance){
+
+            var date = this.selectedDates;
+            console.log(date);
+            serializeData = { ListID: '1', ItemName: 'test' };
+
+            $.ajax({
+                url: "/core/searchrefined/",
+                type: "get",
+                data: serializeData,
+                success: function(response) {
+                    $("#ajax-response").empty();
+                    $("#ajax-response").html(response);
+                }
+
+            });
+
+        }*/
+
+    });
+
 });
