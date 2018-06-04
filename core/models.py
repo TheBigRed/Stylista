8a7fbb40d2d0a15dcef9b150c26d88cbaef5d414
Unknown <ramraja.vii@gmail.com>
2018-06-04 05:47:15 -0400

    Create contract model

--- core/models.py
+++ core/models.py
@@ -63,4 +63,12 @@ class Service(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
 
 
+class Contract(models.Model):
+    client = models.ForeignKey(Stylist, models.CASCADE, related_name='client')
+    stylist = models.ForeignKey(Stylist, models.CASCADE, related_name='stylist')
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=25, blank=True)
+    total_price = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
+
 
