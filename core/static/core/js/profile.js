ee239ac162da2e992b917727ef4af3f070dc1b23
Unknown <ramraja.vii@gmail.com>
2018-06-08 04:15:54 -0400

    Pass current service info to confirmation modal
    *Create javascript function for button click event
    *Get text from profile elements & pass to model elements
    *Create test elements

--- core/static/core/js/profile.js
+++ core/static/core/js/profile.js
@@ -7,6 +7,23 @@ $(document).ready(function() {
 
     });
 
+    $("#modalopen").click(function(){
+
+        var serviceName = $(this).parents().eq(1).children().eq(0).children().eq(0).text();
+        var serviceDescription = $(this).parents().eq(1).children().eq(0).children().eq(1).text();
+        var time = $(this).parents().eq(0).children().eq(0).text();
+        var price = $(this).parents().eq(0).children().eq(1).text();
+        console.log(serviceName);
+        $('#jservice').text(serviceName);
+        $('#jdesc').text(serviceDescription);
+        $('#jtime').text(time);
+        $('#jprice').text(price);
+        //var myBookId = $(this).data('id');
+        //$(".modal-body #bookId").val( myBookId );
+        // As pointed out in comments,
+        // it is superfluous to have to manually call the modal.
+        // $('#addBookDialog').modal('show');
+    });
 
 });
 
