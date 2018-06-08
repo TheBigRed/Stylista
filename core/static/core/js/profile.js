406039b032124cbabe7bb39e2463854ba7a48315
Unknown <ramraja.vii@gmail.com>
2018-06-08 04:32:37 -0400

    Remove unneccessary comments

--- core/static/core/js/profile.js
+++ core/static/core/js/profile.js
@@ -18,9 +18,7 @@ $(document).ready(function() {
         $('#jdesc').text(serviceDescription);
         $('#jtime').text(time);
         $('#jprice').text(price);
-        //var myBookId = $(this).data('id');
-        //$(".modal-body #bookId").val( myBookId );
-        // As pointed out in comments,
+
         // it is superfluous to have to manually call the modal.
         // $('#addBookDialog').modal('show');
     });
