4b704f2fce38e4488da13caf5bf6b0b5b4f14df1
Unknown <ramraja.vii@gmail.com>
2018-08-13 05:09:11 -0400

    Load reviews template with data & css
    *Hardcode Stylist id to load all their reviews
    *Create page container
    *Create rows for each review
    *Add css (import fonts & create container format)
    
    #TODO: format review blocks and rating system

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from .models import Review
 
 def reviews(request):
     '''
-    :param request: get request which has user on db session stored in session cookie
-    :return: main page with correct user after authenticate session
+    :param request: reviews
+    :return: page with reviews for Stylist
     '''
-    return render(request, 'reviews/reviews.html')
+
+    reviews_list = Review.objects.filter(stylist_id=10)
+    for r in reviews_list:
+        print("Review Name: {}".format(r.title))
+
+    return render(request, 'reviews/reviews.html', {'reviews_list': reviews_list})
