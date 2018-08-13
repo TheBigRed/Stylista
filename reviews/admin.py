d7c06c2ce490020ed4c54ff8a47036288664f0b1
Unknown <ramraja.vii@gmail.com>
2018-08-13 02:15:11 -0400

    Create Reviews Database
    *Create Review model
    *Migrate model to Reviews database
    *Add to admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'client_id', 'stylist_id', 'title', 'review', 'rating')
+
+
+admin.site.register(Review, ReviewsAdmin)
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from landing.models import Stylist
 
-# Create your models here.
+
+class Review(models.Model):
+    client = models.ForeignKey(Stylist, models.CASCADE, related_name='client_review')
+    stylist = models.ForeignKey(Stylist, models.CASCADE, related_name='stylist_review')
+    confirmed_client = models.BooleanField(default=None, blank=True)
+    title = models.CharField(max_length=25, blank=True)
+    review = models.TextField(blank=True)
+    rating = models.FloatField(blank=True)
+    date = models.DateField()
+    time = models.TimeField()
+    helpful_count = models.IntegerField(default=0, blank=True)
+    unhelpful_count = models.IntegerField(default=0, blank=True)
+    helpful_total_count = models.IntegerField(default=0, blank=True)
+
+    def __str__(self):
+        return self.title
