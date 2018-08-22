cd3c515b9eb815d18b39f9b33aad66289220ef76
Ram Raja <ramraja.vii@gmail.com>
2018-08-22 13:35:02 -0400

    Create Client model & bring all migrations to current
    *Delete table
    *Delete migrations from db from last
    *Fake migrations
    *migrate
    *Create client model

--- landing/admin.py
+++ landing/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Stylist
+from .models import Stylist, Client
 
 
 class StylistaAdmin(admin.ModelAdmin):
     list_display = ('pk', 'first_name', 'last_name', 'email', 'password', 'joined_date')
 
 
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'address', 'email', 'password')
+
+
 admin.site.register(Stylist, StylistaAdmin)
+admin.site.register(Client, ClientAdmin)
--- landing/models.py
+++ landing/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+import uuid
 
 
 class Stylist(models.Model):
@@ -10,3 +13,21 @@ class Stylist(models.Model):
 
     def __str__(self):
         return self.first_name
+
+
+class Client(User):
+    user = models.OneToOneField(User, parent_link=True, on_delete=models.CASCADE)
+    #client_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    address = models.CharField(max_length=50, blank=True)
+    zip_code = models.CharField(max_length=20, default='')
+    city = models.CharField(max_length=25, blank=True)
+    country = models.CharField(max_length=25, blank=True)
+    location = models.CharField(max_length=25, blank=True)
+    phone_number = models.PositiveIntegerField(validators=[RegexValidator(regex='^\+?[1-9]\d{1,14}$')], null=True)
+
+    def __str__(self):
+        return self.acc_test_fn + self.acc_test_ln
+
+    class Meta:
+        verbose_name = 'Client'
+        verbose_name_plural = 'Clients'
