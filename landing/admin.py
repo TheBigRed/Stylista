d12c18b183fdf108e1cf047681028276b65f33d6
Ram Raja <ramraja.vii@gmail.com>
2018-08-23 02:29:19 -0400

    Admin display changes to Client & Stylista
    *Create functions to return User info
    *Create inline & default admin displays for Client

--- landing/admin.py
+++ landing/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Stylist, Client, Stylista
+from django.contrib.auth.models import User
 
 
 class StylistAdmin(admin.ModelAdmin):
@@ -7,13 +9,43 @@ class StylistAdmin(admin.ModelAdmin):
 
 
 class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'username', 'user_firstname', 'user_lastname','zip_code', 'address')
+
+    def username(self, x):
+        return x.user.username
+
+    def user_firstname(self, x):
+        return x.user.first_name
+
+    def user_lastname(self, x):
+        return x.user.last_name
+
+
+class ClientAdminInline(admin.StackedInline):
+    model = Client
+    can_delete = False
+    verbose_name_plural = 'clients'
     list_display = ('pk', 'zip_code', 'address')
 
 
 class StylistaAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'business_name', 'business_id', 'business_phone_number', 'services', 'store_front')
+    list_display = ('pk', 'username', 'business_name', 'business_id', 'services', 'store_front')
+
+    def username(self, x):
+        return x.user.username
+
+    def user_firstname(self, x):
+        return x.user.first_name
+
+    def user_lastname(self, x):
+        return x.user.last_name
 
 
+class UserAdmin(BaseUserAdmin):
+    inlines = (ClientAdminInline,)
+
+admin.site.unregister(User)
 admin.site.register(Stylist, StylistAdmin)
-admin.site.register(Client, ClientAdmin)
 admin.site.register(Stylista, StylistaAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Client, ClientAdmin)
