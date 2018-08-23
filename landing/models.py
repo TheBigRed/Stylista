94c28960f02cab64cce4864e546eb5dc718dc274
Ram Raja <ramraja.vii@gmail.com>
2018-08-22 23:12:27 -0400

    Change Client fields
    *Change phone_number to CharField
    *Change zip_code from attr from default to blank

--- landing/models.py
+++ landing/models.py
@@ -18,14 +18,15 @@ class Stylist(models.Model):
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=50, blank=True)
-    zip_code = models.CharField(max_length=20, default='')
+    zip_code = models.CharField(max_length=20, blank=True)
     city = models.CharField(max_length=25, blank=True)
     country = models.CharField(max_length=25, blank=True)
     location = models.CharField(max_length=25, blank=True)
-    phone_number = models.PositiveIntegerField(validators=[RegexValidator(regex='^\+?[1-9]\d{1,14}$')], null=True)
+    phone_number = models.CharField(validators=[RegexValidator(regex='^\+?[1-9]\d{1,14}$')],
+                                    null=True, blank=True, max_length=15)
 
     def __str__(self):
-        return self.acc_test_fn + self.acc_test_ln
+        return self.user.first_name + self.user.last_name
 
     class Meta:
         verbose_name = 'Client'
