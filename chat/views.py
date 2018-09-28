dbd13ba64b58931c2755d3b3e4123cb8436d0b88
Ram Raja <ramraja.vii@gmail.com>
2018-09-28 03:11:33 -0400

    Wire model data into Messages template
    *Authenticate user
    *Get messages for logged in user (Need to switch to convo's)
    *Get messages associated with Conversation 
    *Template html with model data

--- chat/views.py
+++ chat/views.py
@@ -2,21 +2,32 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from landing.models import Stylist, Client
 from django.urls import reverse
-from .models import Message
+from .models import Message, Conversation
 from django.contrib.auth.models import User
 
 
 def messages(request):
-    messagess = Message.objects.all()
 
-    context = {
+    if request.user.is_authenticated:
+        print("Authorizing messages access for: " + request.user.username)
+        print("Message user ID: " + str(request.user.pk))
+        convo = Conversation.objects.get(sender=request.user.pk)
+        print("Convo received for: " + str(convo))
+        convo_messages = Message.objects.filter(conversation=convo.pk)
+        context = {
 
-        'messages': messagess
+            'messages': convo_messages,
+            'current_user': str(request.user.username)
 
-    }
+        }
 
-    for m in messagess:
-        print(m.sender)
-        print(m.message)
+        for m in convo_messages:
+            if str(m.sender) == str(request.user.username):
+                print("SENT: {}".format(m.message))
+            else:
+                print("RECEIVED: {}".format(m.message))
 
-    return render(request, 'chat/messages.html', context)
+        return render(request, 'chat/messages.html', context)
+
+    else:
+        return HttpResponseRedirect(reverse('landing:login'))
